c12f8b97946668478d716a9c15bf05b77b73532e
cjrojas72 <cjrojas72@gmail.com>
2025-02-01 17:35:09 -0500

    add swiper js to proj

---
+++ src/app/components/movie-slider/movie-slider.component.ts
@@ -0,0 +1,18 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+@Component({
+  selector: 'app-movie-slider',
+  imports: [],
+  templateUrl: './movie-slider.component.html',
+  styleUrl: './movie-slider.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class MovieSliderComponent {
+
+  
+}
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from '../../layout/banner/banner.component';
+import { MovieSliderComponent } from '../../components/movie-slider/movie-slider.component';
 
 @Component({
   selector: 'app-home',
-  imports: [BannerComponent],
+  imports: [BannerComponent, MovieSliderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
